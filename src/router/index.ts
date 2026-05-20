3bc72a06f9357a0b96faaa80774e771e8891d72a
Milton Chithenga <88409286+MarcuzAC@users.noreply.github.com>
2026-05-20 16:21:06 +0200

    Types

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(),
@@ -12,17 +12,17 @@ const router = createRouter({
     {
       path: '/articles',
       name: 'articles',
-      component: () => import('@/views/ArticlesView.vue')
+      component: () => import('../views/ArticlesView.vue')
     },
     {
       path: '/articles/:id',
       name: 'article-detail',
-      component: () => import('@/views/ArticleDetailView.vue')
+      component: () => import('../views/ArticleDetailView.vue')
     },
     {
       path: '/reports',
       name: 'reports',
-      component: () => import('@/views/ReportsView.vue')
+      component: () => import('../views/ReportsView.vue')
     }
   ]
 })
